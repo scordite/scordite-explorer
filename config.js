b97fcaee54e1f6ec6b3b4d4b6e98e43e7cf96d00
scordite <scordite@protonmail.ch>
2019-05-01 00:31:42 +0200

    files added

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = 'https://karbo.club/services/node_web';
+var apiList = ["95.179.160.20:26111", "95.179.141.233:26111"};
+var blockTargetInterval = 60;
+var coinUnits = 100000000;
+var symbol = 'XSD';
+var refreshDelay = 30000;
+var whiteTheme = "css/themes/white-theme.css";
+var nightTheme = "css/themes/dark-theme.css";
+// pools stats by MainCoins
+var poolsStat = 
+	[
+		["egamelite", "199.247.0.152/#"],
+    ];
+var nodesStat = 
+	[
+        ["199.247.0.152/#"],
+    ];
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$node = 'https://explorer.scordite.one';
+return array(
+	'api' => 'https://explorer.scordite.one',
+	'blockTargetInterval' => 60,
+	'coinUnits' => 100000000,
+	'coinDecimals' => 8
+);
